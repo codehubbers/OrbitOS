a54dfa8a14d668fcb20dabf5f35e28b268d2fd76
[\"Ilker\":{...}] <me@theilker.com>
2025-09-24 09:45:00 -0400

    style: update global app styling and theming
    - Enhance theme context integration
    - Improve visual hierarchy across components
    - Add responsive design improvements

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,20 +4,22 @@ import '@/styles/globals.css';
 import { AppProvider } from '@/context/AppContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { NotificationProvider } from '@/system/services/NotificationRegistry';
-import { SettingsProvider } from '@/context/SettingsContext'; // Corrected the path
+import { SettingsProvider } from '@/context/SettingsContext';
+import { AuthProvider } from '@/context/AuthContext';
 
 // We will use one function for our app wrapper
 export default function MyApp({ Component, pageProps }) {
   return (
-    // All providers are now nested together, wrapping the entire application
     <ThemeProvider>
-      <AppProvider>
-        <NotificationProvider>
-          <SettingsProvider>
-            <Component {...pageProps} />
-          </SettingsProvider>
-        </NotificationProvider>
-      </AppProvider>
+      <AuthProvider>
+        <AppProvider>
+          <NotificationProvider>
+            <SettingsProvider>
+              <Component {...pageProps} />
+            </SettingsProvider>
+          </NotificationProvider>
+        </AppProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
